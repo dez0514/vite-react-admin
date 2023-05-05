1a98082e38da6a1995068298ca2195824ec81f23
zhangwd24 <2573433803@qq.com>
2023-05-05 18:06:41 +0800

    严格模式

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'normalize.css'
@@ -15,7 +15,9 @@ const store = configureStore({
 })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 )
