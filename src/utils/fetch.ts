587f030f370dda70fe130bf9b73b513faed2dbba
zhangwangde <2573433803@qq.com>
2023-05-22 23:18:40 +0800

    fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -5,6 +5,8 @@ import store from '@/reducers/index'
 import { updateConfig } from '@/actions';
 import { parse, compile } from 'path-to-regexp';
 import { notification } from 'antd';
+import { config } from 'process';
+
 /**
  * config 参数配置:
  * abortRequest: boolean 取消之前所有的请求.
@@ -13,7 +15,7 @@ import { notification } from 'antd';
  * url: string 接口地址
  * loading: boolean 是否全局加载
  * hideError: boolean 不走共通的报错
- * hideErrorByCallback 个别code不走共通error时,利用回调判断
+ * hideErrorByCallback：(err) => boolean // 个别code不走共通error时,传说error, 利用error返回code判断是否走共通
  * headers: object header信息覆盖
  * **/
 /**
@@ -26,6 +28,7 @@ const CancelToken = axios.CancelToken
 const CONTENT_TYPE = {
   json: 'application/json;charset=utf-8',
   xwform: 'application/x-www-form-urlencoded',
+  mpform: 'multipart/form-data' // 通常上传用这个
 }
 const defaultConfig = {
   loading: true,
@@ -114,6 +117,16 @@ function handleError(data: any) {
     description: 'xxx'
   })
 }
+// 移除没回来的请求
+function removeSourcesRequest(config: any) {
+	let i = sources.length - 1;
+	for (i; i > -1; i--) {
+		if (sources[i].config === config) {
+			sources.splice(i, 1);
+		}
+	}
+}
+
 const service = axios.create({
   baseURL: baseApiUrl, // api的base_url
   timeout: 30000, // 请求超时时间
@@ -135,6 +148,7 @@ service.interceptors.response.use(
     // 成功请求到数据
     // console.log('response==', response)
     changeLoadingState(response.config, false)
+    removeSourcesRequest(response.config);
     if(!response.config.hideError) {
       handleError(response.data)
     }
@@ -144,7 +158,11 @@ service.interceptors.response.use(
     // 响应错误处理
     // console.log('error==', error)
     changeLoadingState(error.config, false)
-    if(!error.config.hideError) {
+    let hideErr = false
+    if(error.config.hideErrorByCallback && typeof error.config.hideErrorByCallback === 'function') {
+      hideErr = error.config.hideErrorByCallback(error)
+    }
+    if(!hideErr && !error.config.hideError) {
       handleError(error.data)
     }
     return Promise.reject(error)
@@ -153,261 +171,169 @@ service.interceptors.response.use(
 export { service, CancelToken }
 
 
-// request(options) {
-//   const instance = axios.create();
-//   const config = {
-//     baseURL: this.baseURL,
-//     timeout: this.timeout,
-//     ...options
-//   };
-//   this.setInterceptor(instance, options.url); // 给这个实例增加拦截功能
-//   return instance(config); // 返回的是一个promise
-// }
-// /**
-//  * 保存资源
-//  * @param {Object} url
-//  * @param {Object} config
-//  */
-// get(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'get',
-//     url,
-//     params: data
-//   });
-//   return this.request(config);
-// }
-// delete(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'delete',
-//     url,
-//     params: data
-//   });
-//   return this.request(config);
-// }
-// head(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'head',
-//     url,
-//     params: data
-//   });
-//   return this.request(config);
-// }
-// options(url, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'options',
-//     url
-//   });
-//   return this.request(config);
-// }
-// post(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'post',
-//     url,
-//     data
-//   });
-//   return this.request(config);
-// }
-// postUpload(url, data, config = {}) {
-//   config = _.merge(config, {
-//     method: 'post',
-//     url,
-//     data,
-//     headers: {
-//       'Content-Type': 'multipart/form-data'
-//     }
-//   });
-//   return this.request(config);
-// }
-// put(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'put',
-//     url,
-//     data
-//   });
-//   return this.request(config);
-// }
-// patch(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'patch',
-//     url,
-//     data
-//   });
-//   return this.request(config);
-// }
-// // get获取流
-// getBlob(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'get',
-//     url,
-//     params: data,
-//     responseType: 'blob'
-//   });
-//   return this.request(config);
-// }
-// // get下载
-// getDownloadBlob(url, data, config = {}) {
-//   return this.getBlob(url, data, config).then(res => {
-//     download(res);
-//     return res;
-//   });
-// }
-// // get 下载url
-// getDownloadUrl(url, data, config = {}) {
-//   return this.get(url, data, config).then(res => {
-//     // console.log(res);
-//     downloadUrl(res);
-//     return res;
-//   });
-// }
+export const get = (url: string, params: any, option: any) => {
+  const config = {
+    method: 'get',
+    url,
+    params,
+    ...option
+  };
+  return axios.request(config);
+}
+export const getBlob = (url: string, params: any, option: any) => {
+  const config = {
+    method: 'get',
+    url,
+    params,
+    ...option,
+    responseType: 'blob'
+  };
+  return axios.request(config);
+}
 
-// // post获取流
-// postBlob(url, data, config = {}) {
-//   config = Object.assign(config, {
-//     method: 'post',
-//     url,
-//     data,
-//     responseType: 'blob'
-//   });
-//   return this.request(config);
-// }
-// // post下载流
-// postDownloadBlob(url, data, config = {}) {
-//   return this.postBlob(url, data, config).then(res => {
-//     download(res);
-//     return res;
-//   });
-// }
-// // post 下载url
-// postDownloadUrl(url, data, config = {}) {
-//   return this.post(url, data, config).then(res => {
-//     downloadUrl(res);
-//     return res;
-//   });
-// }
+export const download = (res: any, file: string = '') => {
+	const a = document.createElement('a');
+	const blob = URL.createObjectURL(res.data);
+	const filename =
+		file || (res.request.getResponseHeader('content-disposition') && res.request.getResponseHeader('content-disposition').split('filename=')[1]) || '';
+	a.href = blob;
+	a.download = filename ? window.decodeURI(filename) : '文件.xlsx';
+	a.click();
+	window.URL.revokeObjectURL(blob);
+}
+export const downloadUrl = (url: string) => {
+	const a = document.createElement('a');
+  a.href = url
+	a.click();
+};
 
+export const getDownloadBlob = (url: string, params: any, option: any, filename: string) => {
+  return getBlob(url, params, option).then((res: any) => {
+    download(res, filename);
+    return res;
+  });
+}
 
-// export const responseFailedValidate = function(result, e) {
-// 	const msgs = [];
-// 	if (result.actionErrors) {
-// 		result.actionErrors.forEach(err => {
-// 			msgs.push([err.message, handleParams(err)]);
-// 		});
-// 	}
-// 	if (result.actionMessages) {
-// 		result.actionMessages.forEach(err => {
-// 			msgs.push([err.message, handleParams(err)]);
-// 		});
-// 	} else if (result.fieldErrors) {
-// 		const fieldErrors = result.fieldErrors;
-// 		for (const prop in fieldErrors) {
-// 			msgs.push([
-// 				fieldErrors[prop][0].message,
-// 				handleParams(fieldErrors[prop][0])
-// 			]);
-// 		}
-// 	}
-// 	// 提示信息
-// 	msgs.forEach((msg, idx) => {
-// 		if (messageCache.includes(msg[0])) return;
-// 		messageCache.push(msg[0]);
-// 		setTimeout(() => {
-// 			messageCache.splice(messageCache.findIndex(m => m === msg[0]), 1);
-// 		}, duration);
-// 		Notification({
-// 			title: i18n.t('message.warn'),
-// 			message: i18n.t(`message['${msg[0]}']`, msg[1]),
-// 			type: 'error',
-// 			offset: 65 * idx
-// 		});
-// 		if (
-// 			msg[0] == 'auth.access.token.invalid' ||
-//       msg[0] == 'auth.access.token.expired'
-// 		) {
-// 			router.push('/login');
-// 			store.dispatch('user/resetInfo', null, {
-// 				root: true
-// 			});
-// 			window.location.reload();
-// 		}
-// 	});
-// };
+export const post = (url: string, data: any, option: any) => {
+  const config = {
+    method: 'post',
+    url,
+    data: data,
+    ...option
+  };
+  return axios.request(config);
+}
+export const postBlob = (url: string, data: any, option: any) => {
+  const config = {
+    method: 'post',
+    url,
+    data,
+    ...option,
+    responseType: 'blob'
+  };
+  return axios.request(config);
+}
+export const postDownloadBlob = (url: string, data: any, option: any, filename: string) => {
+  return postBlob(url, data, option).then((res) => {
+    download(res, filename);
+    return res;
+  });
+}
+export const postDownloadUrl = (url: string, data: any, option: any) => {
+  return post(url, data, option).then((res: any) => {
+    downloadUrl(res);
+    return res;
+  });
+}
+export const getDownloadUrl = (url: string, data: any, option: any) => {
+  return get(url, data, option).then((res: any) => {
+    downloadUrl(res);
+    return res;
+  });
+}
+export const deletes = (url: string, params: any, option: any) => {
+  const config = {
+    method: 'delete',
+    url,
+    params,
+    ...option
+  };
+  return axios.request(config);
+}
+export const head = (url: string, params: any, option: any) => {
+  const config = {
+    method: 'head',
+    url,
+    params,
+    ...option
+  };
+  return axios.request(config);
+}
+export const options = (url: string, option: any) => {
+  const config = {
+    method: 'options',
+    url,
+    ...option
+  };
+  return axios.request(config);
+}
 
-// const handleParams = function(err) {
-// 	const pageId = util.getSessionStorage('pageId') || 'login';
-// 	const obj = {};
-// 	obj.filed = i18n.t(`${pageId}['${err.filed}']`);
-// 	if (obj.filed.startsWith(pageId)) {
-// 		obj.filed = i18n.t(`filed['${err.filed}']`);
-// 	}
-// 	if (obj.filed.startsWith('filed')) {
-// 		obj.filed = err.filed;
-// 	}
-// 	// 对报错信息中的params中信息进行翻译
-// 	const params = err.params;
-// 	// 获取params中所有的key值，然后进行for循环
-// 	Object.keys(params).forEach(key => {
-// 		// 1、将zh.js相关页面模块下面的翻译赋值给obj[key]
-// 		obj[key] = i18n.t(`${pageId}['${params[key]}']`);
-// 		// 2、判断obj[key]这个值是不是以pageId开头，如果是以pageId开头说明未翻译成功，此时从message_zh.js文件下的filed中进行翻译
-// 		if (obj[key].startsWith(pageId)) {
-// 			obj[key] = i18n.t(`filed['${params[key]}']`);
-// 		}
-// 		// 3 判断obj[key]这个值是不是已filed开头，如果是则说明从message_zh.js翻译还是没成功，此时直接返回params[key]
-// 		if (obj[key].startsWith('filed')) {
-// 			obj[key] = params[key];
-// 		}
-// 	});
-// 	return obj;
-// };
+export const postUpload = (url: string, data: any, option: any) => {
+  const config = {
+    method: 'post',
+    url,
+    data,
+    headers: {
+      'Content-Type': CONTENT_TYPE.mpform
+    },
+    ...option
+  }
+  return axios.request(config);
+}
+export const put = (url: string, data: any, option: any) => {
+  const config = {
+    method: 'put',
+    url,
+    data,
+    ...option
+  }
+  return axios.request(config);
+}
+export const patch = (url: string, data: any, option: any) => {
+  const config = {
+    method: 'patch',
+    url,
+    data,
+    ...option
+  }
+  return axios.request(config);
+}
 
-// // 下载回调
-// const download = function(res) {
-// 	const a = document.createElement('a');
-// 	const blob = URL.createObjectURL(res.data);
-// 	const filename = (res.request.getResponseHeader('content-disposition') && res.request.getResponseHeader('content-disposition').split('filename=')[1]) || '';
-// 	a.href = blob;
-// 	a.download = filename || '文件.xlsx';
-// 	a.click();
-// 	window.URL.revokeObjectURL(blob);
-// };
-
-// export const downloadUrl = function(res) {
-// 	const a = document.createElement('a');
-// 	if (typeof res === 'string') {
-// 		a.href = res;
-// 	} else if (res.data && typeof res.data === 'string') {
-// 		a.href = res.data;
-// 	} else {
-// 		a.href = res.data.body;
-// 	}
-// 	a.click();
-// };
-
-// export const downloadImg = function(src, cb) {
-// 	var filename; // 图片名称
-// 	var filetype; // 图片类型
-// 	var path = src;
-// 	if (path.indexOf('/') > 0) {
-// 		var file = path.substring(path.lastIndexOf('/') + 1, path.length);
-// 		var fileArr = file.toLowerCase().split('.');
-// 		filename = fileArr[0];
-// 		filetype = fileArr[1];
-// 	}
-// 	var canvas = document.createElement('canvas');
-// 	var img = document.createElement('img');
-// 	img.onload = function(e) {
-// 		canvas.width = img.width;
-// 		canvas.height = img.height;
-// 		var context = canvas.getContext('2d');
-// 		context.drawImage(img, 0, 0, img.width, img.height);
-// 		canvas.toBlob(blob => {
-// 			var a = document.createElement('a');
-// 			a.href = window.URL.createObjectURL(blob);
-// 			a.download = filename;
-// 			a.click();
-// 			cb && cb();
-// 		}, `image/${filetype}`);
-// 	};
-// 	img.setAttribute('crossOrigin', 'Anonymous');
-// 	img.src = src;
-// };
+export const downloadImg = function(src: string, cb: Function) {
+	let filename: string = '' // 图片名称
+	let filetype: string = '' // 图片类型
+	let path = src;
+	if (path.indexOf('/') > 0) {
+		let file = path.substring(path.lastIndexOf('/') + 1, path.length);
+		let fileArr = file.toLowerCase().split('.');
+		filename = fileArr[0];
+		filetype = fileArr[1];
+	}
+	let canvas = document.createElement('canvas');
+	let img = document.createElement('img');
+	img.onload = function() {
+		canvas.width = img.width;
+		canvas.height = img.height;
+		const context: CanvasRenderingContext2D | null = canvas.getContext('2d');
+		context!.drawImage(img, 0, 0, img.width, img.height);
+		canvas.toBlob((blob: Blob | null) => {
+			let a = document.createElement('a');
+			a.href = window.URL.createObjectURL(blob!);
+			a.download = filename;
+			a.click();
+			cb && cb();
+		}, `image/${filetype}`);
+	};
+	img.setAttribute('crossOrigin', 'Anonymous');
+	img.src = src;
+};
