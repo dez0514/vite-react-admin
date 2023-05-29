25cd4a663171df1fadb91db92a587558bf6b3bd9
dez0514 <dez0514@users.noreply.github.com>
2023-05-29 07:47:02 +0000

    deploy: be7a121e713517ada7a2cc78c91f1f937fe01a87

---
+++ assets/index-4cd94900.js
@@ -0,0 +1,1 @@
+import{j as s,S as a,B as r}from"./index-babd45cb.js";function t(){return s.jsx("div",{className:"tags",children:s.jsx("div",{children:s.jsx(a,{children:s.jsx(r,{type:"primary",children:"Tags"})})})})}export{t as default};
