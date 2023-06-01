cacf698e6987d4a7a1ff26a78c71f993b1879ae9
zhangwd24 <2573433803@qq.com>
2023-06-01 19:05:28 +0800

    count

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react'
 import CustomColorPicker from '@/components/colorPicker'
 import { useSelector, shallowEqual } from 'react-redux'
 import { GlobalConfigState } from '@/types/reducer';
+import MyCard from './components/card/card';
 
 function Home() {
   const navigate = useNavigate();
@@ -18,6 +19,48 @@ function Home() {
   const gorouter = () => {
     navigate(`/pics`)
   }
+  const cardList = [
+    {
+      id : 1,
+      end : 6666,
+      duration : 3,
+      title : 'Order',
+      icon : 'yu-icon-lifangti',
+      badge : '+56%',
+      info : 'From previous period',
+      prefix : '￥'
+    },
+    {
+      id : 2,
+      end : 95270,
+      duration : 3,
+      prefix : '￥',
+      title : 'Income',
+      icon : 'yu-icon-chanpin1',
+      badge : '+78%',
+      info : 'From previous period'
+    },
+    {
+      id : 3,
+      end : 666,
+      duration : 3,
+      prefix : '￥',
+      title : 'Average Price',
+      icon : 'yu-icon-jiagebiaoqian',
+      badge : '-5.2%',
+      info : 'From previous period'
+    },
+    {
+      id : 4,
+      end : 9527,
+      duration : 3,
+      title : 'Product Sold',
+      icon : 'yu-icon-icon_xinyong_xianxing_jijin-129',
+      badge : '+22%',
+      prefix : '￥',
+      info : 'From previous period'
+    }
+  ]
   // useEffect(() => {
   //   const source = new EventSource('http://localhost:3000/article');
   //   let str = ''
@@ -39,6 +82,14 @@ function Home() {
         <Space>
           <Button type="primary" onClick={gorouter}>Home</Button>
         </Space>
+        {
+          cardList.map((item: any, index: number) => {
+            const { end, duration, title, prefix, badge, info } = item
+            return (
+              <MyCard key={index} start={0} end={end} prefix={prefix} info={info} badge={badge} title={title} duration={duration} />
+            )
+          })
+        }
         {/* <div className='tw-bg-orange-400 tw-text-green-700'>{msg}</div> */}
         <CustomColorPicker hexval={color} onChange={colorChange} />
       </div>
