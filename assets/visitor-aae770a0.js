0d4f61991bc423c577bcfd79a51c8f96fde96f0c
dez0514 <dez0514@users.noreply.github.com>
2023-06-05 06:16:25 +0000

    deploy: 1603a53ee3f9e2e4e53d5e8b266b8575b1af5c1b

---
+++ assets/visitor-aae770a0.js
@@ -0,0 +1,1 @@
+import{j as t,P as i}from"./index-0924a979.js";import{T as r}from"./typingCard-120717d6.js";function a(){const s="这个页面只有admin和visitor角色才可以访问，editor角色看不到";return t.jsx(i,{className:"VisitorTest",children:t.jsx(r,{title:"visitor页面",source:s})})}export{a as default};
